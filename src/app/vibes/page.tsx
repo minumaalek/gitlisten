ffd203bba43e3c5b4213176b812f46dcb5bca34a
minumaalek <minumaalek@gmail.com>
2026-07-02 20:11:44 +0330

    vibes list fixed

--- src/app/vibes/page.tsx
+++ src/app/vibes/page.tsx
@@ -6,11 +6,13 @@ import VibesSearchBox from "@/components/vibes-search-box";
 
 export default function Vibe() {
   return (
-    <div className="flex flex-col items-center md:items-start md:grid md:grid-cols-[1fr_3fr] p-1 h-full w-full">
-      <div className="flex flex-col gap-2 border w-full justify-center items-center">
+    <div className="flex flex-col items-center md:items-start md:grid md:grid-cols-[1fr_3fr] gap-5 p-1 h-full w-full">
+      <div className="flex flex-col gap-2 w-full justify-center items-center">
         <h2>All vibes around the git</h2>
         <VibesSearchBox />
-        <VibesList />
+        <div className="w-full h-[29vw] overflow-y-auto">
+          <VibesList />
+        </div>
       </div>
       <VibeCard />
     </div>
--- src/components/vibes-list.tsx
+++ src/components/vibes-list.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 export default function VibesList() {
-  const vibe = (
-    <li className="flex justify-between items-center bg-gray-200 w-7/8 p-3 cursor-pointer hover:bg-gray-300">
-      <div className="flex">
-        <div>💔</div>
-        <Link href={"/"}>Explain</Link>
-      </div>
-      <i className="text-sm">minu987</i>
-    </li>
-  );
   return (
-    <ul className="flex flex-col gap-2 w-full">
-      {[...Array(3)].map((_, index) => {
-        return <div key={index}>{vibe}</div>;
+    <ul className="flex flex-col gap-2 w-full items-center">
+      {[...Array(20)].map((_, index) => {
+        return (
+          <li className="flex justify-between items-center bg-gray-200 w-full p-3 cursor-pointer hover:bg-gray-300">
+            <div className="flex">
+              <div>💔</div>
+              <Link href={"/"}>Explain</Link>
+            </div>
+            <i className="text-sm">minu987</i>
+          </li>
+        );
       })}
     </ul>
   );
